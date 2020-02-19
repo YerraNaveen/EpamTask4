f0c2006cacf5295b5eb6e5ad5630f0d18cd511b8
Naveen-coder <yerranaveen777@gmail.com>
2020-02-19 23:47:37 +0530

    Clean Code

---
+++ src/main/java/Cmr/yerra/HomeTask.java
@@ -0,0 +1,11 @@
+package Cmr.yerra;
+
+public class HomeTask 
+{
+    public static void main( String[] args )
+    {
+        InterestEvaluation.interestEvaluator();
+        ConstructionCostEstimation.costEvaluator(); 
+       
+    }
+}
